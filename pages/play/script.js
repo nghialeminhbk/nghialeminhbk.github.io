bc86ce669f2b6a6017c61a77480ef8f00fbe8114
Le Minh Nghia <nghia9c.12@gmail.com>
2022-02-01 15:44:19 +0700

    up boc lixi

--- pages/play/script.js
+++ pages/play/script.js
@@ -16,6 +16,19 @@ for(var i=1; i<=3; i++){
     array.push("100.000");
 }
 
+function loading(){
+    var textNoti = document.getElementById("text-noti")
+    var loading = document.getElementById("loading")
+    var audio =  document.getElementById("audio")
+    audio.play();
+    textNoti.setAttribute("hidden", "");
+    loading.removeAttribute("hidden");
+    setTimeout(()=>{
+        getRandom();
+        audio.pause();
+    }, 18000);
+}
+
 function getRandom() {
     var numberGen = document.getElementById("number")
     numberGen.innerHTML = `<span>Chúc mừng bạn nhận được lì xì mệnh giá</span> ( ` + array[Math.floor((Math.random() * 33))]  + ` đồng ) !!!`;
